1fa4c36385025a65c2cfb35c94e9f8ac35789894
JerryZhou54 <zhouw.jerry2017@outlook.com>
2025-04-22 22:31:43 -0400

    update

--- run_old_train.sh
+++ run_old_train.sh
@@ -1,7 +1,19 @@
-# Train on TuringBench dataset
+# Run training on GPT-2 Output Dataset
+REAL_DATASET='webtext'
+FAKE_DATASET='small-117M' # Choose from 'small-117M', 'large-762M', 'xl-1542M'
 python old_detector/train.py \
     --max-epochs 1 \
     --batch-size 64 \
     --max-sequence-length 510 \
-    --data-dir /home/hice1/wzhou322/scratch/TuringBench/AA \
-    --learning-rate 3e-6
+    --data-dir /home/hice1/wzhou322/scratch/gpt2-output-data \
+    --real-dataset $REAL_DATASET \
+    --fake-dataset $FAKE_DATASET
+
+# Train on TuringBench dataset
+# python old_detector/train.py \
+#     --max-epochs 1 \
+#     --batch-size 64 \
+#     --max-sequence-length 510 \
+#     --data-dir /home/hice1/wzhou322/scratch/TuringBench/AA \
+#     --learning-rate 3e-6 \
+#     --ckpt YOUR_CKPT_DIR
